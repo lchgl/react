3e81345a7858a915c7ebd383a8620601888e27a3
lchgl <403552501@qq.com>
2020-03-24 09:49:03 +0800

    —全局引入样式

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+//重写覆盖next的app.js需重新dev
+import App from 'next/app'
+
+// 全局引入样式
+import 'antd/dist/antd.css'
+import '../static/style/pages/comm.css'
+
+export default App
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 import Head from 'next/head'
 
+import { Button } from 'antd'
+
 const Home = () => {
   return (
-    <Head>
-      <title>Home</title>
-    </Head>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div>
+        <Button type='primary'>btn</Button>
+      </div>
+    </>
   )
 }
 
